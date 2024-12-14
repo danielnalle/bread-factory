4d38e03c0723856cb07a2baeeea56fbc268feb28
Daniel Nalle <danielnalle31@gmail.com>
2024-12-14 18:38:17 +0800

    memodifikasi dan membuat migratios

---
+++ database/migrations/2024_12_14_090056_create_breads_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('breads', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->foreignId('bread_type_id')->constrained('bread_types')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->integer('min_order');
+            $table->decimal('price');
+            $table->date('produce_date');
+            $table->date('expiry_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('breads');
+    }
+};
---
+++ database/migrations/2024_12_14_103448_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
+            $table->foreignId('order_status_id')->constrained('order_status')->onDelete('cascade');
+            $table->foreignId('payment_method_id')->constrained('payment_methods')->onDelete('cascade');
+            $table->foreignId('payment_status_id')->constrained('payment_status')->onDelete('cascade');
+            $table->date('order_date');
+            $table->integer('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
