fb843b402c8c15294c13d9931b60e45df9eee077
yogipradnyana <15tkj1yogipradnyana@gmail.com>
2024-12-24 09:32:01 +0800

    Menambahkan fitur read dan delete category pada menu Kategori

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([UserSeeder::class]);
+        $this->call([UserSeeder::class, CategorySeeder::class]);
     }
 }
