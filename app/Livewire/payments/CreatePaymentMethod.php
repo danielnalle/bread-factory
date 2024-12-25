1530f0701bd8ffd65ea9f76d533d5ab8f9ab3456
yogipradnyana <15tkj1yogipradnyana@gmail.com>
2024-12-25 08:16:24 +0800

    Menambahkan fitur create payment method pada Menu Metode Pembayaran dan menambahkan trigger log_breads pada table breads untuk create, update dan delete

---
+++ app/Livewire/payments/CreatePaymentMethod.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire\Payments;
+
+use App\Models\PaymentMethod;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class CreatePaymentMethod extends Component
+{
+    #[Validate('required|string|max:255|unique:payment_methods,name')]
+    public $name;
+    #[Validate('boolean')]
+    public $isActive = false;
+
+    public function render()
+    {
+        return view('livewire.payments.create-payment-method');
+    }
+
+    public function store()
+    {
+        $this->validate();
+
+        PaymentMethod::create([
+            'name' => $this->name,
+            'isActive' => $this->isActive,
+        ]);
+
+        flash("Metode Berhasil Ditambah", "success");
+        return redirect()->route('payment-method');
+    }
+}
--- resources/views/livewire/payments/create-payment-method.blade.php
+++ resources/views/livewire/payments/create-payment-method.blade.php
@@ -1,12 +1,12 @@
-<form class="max-w-full mx-auto" wire:submit='update'>
+<form class="max-w-full mx-auto" wire:submit='store'>
     <div class="md:grid md:grid-cols-2">
         <div class="flex flex-col gap-5">
             <div>
-                <label for="name" class="block mb-2.5 text-sm font-medium text-dark-primary dark:text-white">Nama<span
-                        class="text-red-600">*</span></label>
+                <label for="name" class="block mb-2.5 text-sm font-medium text-dark-primary dark:text-white">Nama
+                    Metode Pembayaran<span class="text-red-600">*</span></label>
                 <input type="text" id="name"
                     class=" border border-gray-300 text-dark-primary text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-3 placeholder-dark-secondary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
-                    placeholder="Masukan nama kategori" wire:model='name' />
+                    placeholder="Masukan metode pembayaran" wire:model='name' />
                 @error('name')
                     <span class="text-red-600 text-sm">{{ $message }}</span>
                 @enderror
@@ -17,8 +17,7 @@
                     Active<span class="text-red-600">*</span></label>
 
                 <label class="inline-flex items-center cursor-pointer mb-5">
-                    <input type="checkbox" name="active" id="active"
-                        @if ($isActive) checked @endif wire:model='isActive' class="sr-only peer">
+                    <input type="checkbox" name="active" id="active" wire:model='isActive' class="sr-only peer">
                     <div
                         class="relative w-11 h-6 bg-gray-200  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary">
                     </div>
@@ -32,8 +31,8 @@
 
     <button type="submit"
         class="text-white bg-primary hover:bg-primary-hover font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-primary dark:hover:bg-primary-hover ">
-        Simpan
+        Tambah
     </button>
-    <a href="{{ route('categories') }}"
+    <a href="{{ route('payment-method') }}"
         class="text-dark-primary bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Batal</a>
 </form>
