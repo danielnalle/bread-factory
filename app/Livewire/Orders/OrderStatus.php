4ab3f5f698e345058318d60020a27cdd4224dbba
yogipradnyana <15tkj1yogipradnyana@gmail.com>
2025-01-23 00:41:18 +0800

    Memperbaiki Order Status Notification

--- app/Livewire/Orders/OrderStatus.php
+++ app/Livewire/Orders/OrderStatus.php
@@ -3,6 +3,11 @@
 namespace App\Livewire\Orders;
 
 use App\Models\Order;
+use App\Models\OrderStatus as ModelsOrderStatus;
+use App\Models\User;
+use App\Notifications\OrderStatusNotification;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 use Livewire\Component;
 
 class OrderStatus extends Component
@@ -24,6 +29,11 @@ class OrderStatus extends Component
             'order_status_id' => (int) $this->orderStatusId,
         ]);
 
+        $userId = $this->order->cart->user->id;
+        $userOrder = User::where('id', $userId)->get();
+        $statusName = ModelsOrderStatus::find($orderStatusId)->name;
+        $statusName = strtolower($statusName);
+        Notification::send($userOrder, new OrderStatusNotification($this->order, 'Pesanan Update', $statusName . '.'));
         flash('Status Berhasil Diupdate', 'success');
         return redirect()->route('orders.detail', $this->order->no_order);
     }
